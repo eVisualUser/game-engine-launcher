ba7f6caf450a7723613cab3069fbf4fa468810c4
eVisualUser <sylvain.tosoni.dev@protonmail.com>
2024-09-19 00:12:53 +0200

    feat: first version

--- game-launcher/src-tauri/src/lib.rs
+++ game-launcher/src-tauri/src/lib.rs
@@ -1,14 +1,73 @@
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+
+use pretty_ini::*;
+
+static DATA_FILE_PATH: &'static str = "./data.ini";
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[tauri::command]
+fn get_saved(key: &str) -> String {
+    let mut file = ini_file::IniFile::default();
+    file.set_path(DATA_FILE_PATH);
+
+    let mut ini = ini::Ini::default();
+    ini.load(&mut file).unwrap();
+
+    match ini.get_ref(ini::TABLE_NAME_ROOT, key) {
+        Ok(v) => v.value.clone(),
+        Err(_e) => String::new(),
+    }
+}
+
+#[tauri::command]
+fn set_saved(key: &str, value: &str) {
+    let mut file = ini_file::IniFile::default();
+    file.set_path(DATA_FILE_PATH);
+
+    let mut ini = ini::Ini::default();
+    ini.load(&mut file).unwrap();
+
+    match ini.get_ref_mut(ini::TABLE_NAME_ROOT, key) {
+        Ok(v) => v.set(value.to_string()),
+        Err(_e) => {}
+    }
+
+    file.save(&mut ini);
+}
+
+#[tauri::command]
+async fn run_cmd(program: &str, args: &str) -> Result<(), ()> {
+    match std::process::Command::new(program)
+        .args(args.split(' '))
+        .output()
+    {
+        Ok(o) => {
+            println!(
+                "Command stdout result: {}",
+                String::from_utf8_lossy(&o.stdout)
+            );
+            println!(
+                "Command stderr result: {}",
+                String::from_utf8_lossy(&o.stderr)
+            );
+        }
+        Err(e) => eprintln!("Command failure: {}", e),
+    }
+
+    Ok(())
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            greet, get_saved, set_saved, run_cmd
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- game-launcher/src/main.js
+++ game-launcher/src/main.js
@@ -1,18 +1,87 @@
 const { invoke } = window.__TAURI__.core;
 
-let greetInputEl;
-let greetMsgEl;
+let enginePath;
+let projectPath;
 
 async function greet() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
   greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
+async function get_saved(input_key) {
+  console.log("Get Saved")
+  return await invoke("get_saved", { key: input_key });
+}
+
+async function set_saved(input_key, input_value) {
+  console.log("Set saved")
+  await invoke("set_saved", { key: input_key, value: input_value });
+}
+
+async function run_cmd(cmd, args_) {
+  console.log("Execute command: " + cmd + " " + args_)
+  await invoke("run_cmd", { program: cmd, args: args_ });
+}
+
+let enginePathStr = "";
+let projectPathStr = "";
+
+let buttonCompile = null;
+let buttonLaunch = null;
+
+function SetButtonsState(newState) {
+  if (!newState) {
+    buttonCompile.disabled = true;
+    buttonLaunch.disabled = true;
+
+    buttonCompile.style.opacity = '0.5';
+    buttonLaunch.style.opacity = '0.5';
+  } else {
+    buttonCompile.style.opacity = '1.0';
+    buttonLaunch.style.opacity = '1.0';
+    buttonCompile.disabled = false;
+    buttonLaunch.disabled = false;
+  }
+}
+
+window.addEventListener("DOMContentLoaded", async () => {
+  enginePath = document.querySelector("#engine-path");
+  projectPath = document.querySelector("#project-path");
+
+  enginePathStr = await get_saved("engine_path");
+  projectPathStr = await get_saved("project_path");
+
+  enginePath.value = enginePathStr;
+  projectPath.value = projectPathStr; 
+
+  buttonCompile = document.querySelector("#button-compile");
+  buttonLaunch = document.querySelector("#button-launch");
+
+  buttonCompile.addEventListener('click', async function() {
+    SetButtonsState(false);
+    document.querySelector("#p-state").textContent = "Compiling...";
+    await run_cmd(enginePathStr + "/Engine/build/BatchFiles/RunUAT.bat", 'BuildCookRun -project=' + projectPathStr + ' -targetplatform=Win64 -configuration=Development -cook -build -run');
+    document.querySelector("#p-state").textContent = "Compiled";
+    SetButtonsState(true);
+  });
+
+  buttonLaunch.addEventListener('click', async function() {
+    SetButtonsState(false);
+    document.querySelector("#p-state").textContent = "Running editor";
+    await run_cmd(enginePathStr + "/Engine/Binaries/Win64/UnrealEditor.exe", projectPathStr);
+    document.querySelector("#p-state").textContent = "Stopped editor";
+    SetButtonsState(true);
+  });
+
+  console.log("Loaded engine path: " + enginePathStr);
+  console.log("Loaded project path: " + projectPathStr);
+
+  document.querySelector("#launch-form").addEventListener("submit", (e) => {
     e.preventDefault();
-    greet();
+    console.log("Submit")
+    enginePathStr = enginePath.value;
+    projectPathStr = projectPathStr;
+    set_saved("engine_path", enginePath.value);
+    set_saved("project_path", projectPath.value);
   });
 });
